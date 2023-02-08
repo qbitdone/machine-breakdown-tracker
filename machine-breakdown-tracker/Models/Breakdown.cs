ff7470175ca34b380a95715242ce6837e72382e2
Kristijan <kculina@protonmail.ch>
2023-02-08 19:20:02 +0100

    Add Breakdown model

---
+++ machine-breakdown-tracker/Models/Breakdown.cs
@@ -0,0 +1,15 @@
+﻿namespace machine_breakdown_tracker.Models
+{
+    public class Breakdown
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Machine { get; set; }
+        public string Priority { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; }
+        public bool Eliminated { get; set; }
+
+    }
+}
