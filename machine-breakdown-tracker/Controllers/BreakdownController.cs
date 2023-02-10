a946bfa8906e76b222cbda300938ebc2b85a23d6
Kristijan <kculina@protonmail.ch>
2023-02-10 23:27:08 +0100

    Create BreakdownService; Create endpoint for getting all breakdowns

---
+++ machine-breakdown-tracker/Controllers/BreakdownController.cs
@@ -0,0 +1,21 @@
+﻿using machine_breakdown_tracker.Models;
+using machine_breakdown_tracker.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace machine_breakdown_tracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BreakdownController : ControllerBase
+    {
+        private readonly IBreakdownService _breakdownService;
+        public BreakdownController(IBreakdownService breakdownService)
+        {
+            _breakdownService = breakdownService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Machine>>> GetAllMachines() => Ok(await _breakdownService.GetAllBreakdowns());
+    }
+}
--- machine-breakdown-tracker/Services/BreakdownService.cs
+++ machine-breakdown-tracker/Services/BreakdownService.cs
@@ -1,12 +1,21 @@
-﻿using machine_breakdown_tracker.Models;
+﻿using Dapper;
+using machine_breakdown_tracker.Context;
+using machine_breakdown_tracker.Models;
+using System.Data;
 
 namespace machine_breakdown_tracker.Services
 {
     public class BreakdownService : IBreakdownService
     {
-        public Task<IEnumerable<Breakdown>> GetAllBreakdowns()
+        private readonly DapperContext _context;
+        private IDbConnection _connection;
+
+        public BreakdownService(DapperContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _connection = _context.CreateConnection();
         }
+        public async Task<IEnumerable<Breakdown>> GetAllBreakdowns() => await _connection.QueryAsync<Breakdown>("SELECT * FROM breakdown");
+
     }
 }
